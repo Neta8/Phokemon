bfc33dca4f6879d2c5947ce3e9aec15cca09f9ca
Neta8 <noety_pokemon@hotmail.com>
2019-06-30 18:35:46 +0200

    Add files via upload

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
+import pygame
+from pygame.locals import *
 from Constants import *
-from Models import *
+from Models.Battle import *
+from Models.Pokemon import *
 
-pokemon1 = Pokemon("Bulbasaur", 100, "grass", "poison")
-pokemon2 = Pokemon("Charmander", 100, "fire", None)
+pokemon1 = Pokemon("Bulbasaur", 100, 11, 3)
+pokemon2 = Pokemon("Blaziken", 100, 9, 1)
 pokemon1.current_hp = 45
-pokemon2.current_hp = 39
+pokemon2.current_hp = 45
 
-pokemon1.stats = {
+pokemon1.baseStats = {
     HP: 45,
     ATK: 49,
     DEF: 49,
@@ -14,17 +17,53 @@ pokemon1.stats = {
     SPDEF: 65,
     SPEED: 45
 }
-pokemon2.stats = {
-    HP: 39,
-    ATK: 52,
-    DEF: 43,
-    SPATK: 80,
-    SPDEF: 65,
-    SPEED: 65
+pokemon1.ev = {
+    HP: 0,
+    ATK: 0,
+    DEF: 0,
+    SPATK: 0,
+    SPDEF: 0,
+    SPEED: 0
 }
+pokemon1.iv = {
+    HP: 21,
+    ATK: 21,
+    DEF: 21,
+    SPATK: 21,
+    SPDEF: 21,
+    SPEED: 21
+}
+pokemon1.compute_stats()
+print(pokemon1.stats)
+pokemon2.baseStats = {
+    HP: 45,
+    ATK: 49,
+    DEF: 49,
+    SPATK: 65,
+    SPDEF: 65,
+    SPEED: 45
+}
+pokemon2.ev = {
+    HP: 0,
+    ATK: 0,
+    DEF: 0,
+    SPATK: 0,
+    SPDEF: 0,
+    SPEED: 0
+}
+pokemon2.iv = {
+    HP: 21,
+    ATK: 21,
+    DEF: 21,
+    SPATK: 21,
+    SPDEF: 21,
+    SPEED: 21
+}
+pokemon2.compute_stats()
+print(pokemon2.stats)
 
-pokemon1.attacks = [Attack("SCRATCH", "normal", PHYSICAL, 10, 10, 100)]
-pokemon2.attacks = [Attack("SCRATCH", "normal", PHYSICAL, 10, 10, 100)]
+pokemon1.attacks = [Attack("TAIL WHIP", 11, PHYSICAL, 10, 10, 100)]
+pokemon2.attacks = [Attack("SCRATCH", 0, PHYSICAL, 10, 10, 100)]
 
 battle = Battle(pokemon1, pokemon2)
 
@@ -42,14 +81,65 @@ def ask_command(pokemon):
     return command
 
 
-while not battle.is_finished():
-    command1 = ask_command(pokemon1)
-    command2 = ask_command(pokemon2)
+def load_resources():
+    load_pokemon_image(pokemon1, True)
+    load_pokemon_image(pokemon2, False)
 
-    turn = Turn()
-    turn.command1 = command1
-    turn.command2 = command2
 
-    if turn.can_start():
-        battle.execute_turn(turn)
-        battle.print_current_status()
+def render(screen):
+    screen.fill((255,255,255))
+    render_pokemon(screen, pokemon1, pokemon2)
+    pygame.display.update()
+
+
+def update():
+    pass
+
+
+def load_pokemon_image(pokemon, is_player):
+    pokemon_name = pokemon.name.lower()
+    if is_player:
+        pokemon_img = pygame.image.load('Resources/Pokemon/'+pokemon_name+"_back.png")
+        pokemon_img = pygame.transform.scale(pokemon_img, (400, 400))
+        pokemon.renderer = pokemon_img
+    else:
+        pokemon_img = pygame.image.load('Resources/Pokemon/'+pokemon_name+".png")
+        pokemon_img = pygame.transform.scale(pokemon_img, (400, 400))
+        pokemon.renderer = pokemon_img
+
+
+def render_pokemon(screen, pokemon1, pokemon2):
+    pokemon1.render(screen, (10, 200))
+    pokemon2.render(screen, (440, -50))
+
+
+def main():
+    pygame.init()
+    screen = pygame.display.set_mode((800, 640))
+    pygame.display.set_caption("Mc Pokémon")
+    load_resources()
+    clock = pygame.time.Clock()
+    stopped = False
+    clock.tick(60)
+    while not stopped:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                sttoped = True
+        update()
+        render(screen)
+
+
+if __name__ == "__main__":
+    main()
+
+# while not battle.is_finished():
+#    command1 = ask_command(pokemon1)
+#    command2 = ask_command(pokemon2)
+
+#    turn = Turn()
+#    turn.command1 = command1
+#    turn.command2 = command2
+
+#    if turn.can_start():
+#        battle.execute_turn(turn)
+#        battle.print_current_status()
